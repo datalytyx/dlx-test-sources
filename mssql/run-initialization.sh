f518f4d423680176497b05c0c8d74ed6f14f0920
Abhinav Kumar <akumar@datopic.com>
2020-05-25 16:32:24 +0530

    Updated script

--- mssql/run-initialization.sh
+++ mssql/run-initialization.sh
@@ -1,5 +1,5 @@
 # Wait to be sure that SQL Server came up
-sleep 300s
+sleep 30s
 
 # Run the setup script to create the DB and the schema in the DB
 # Note: make sure that your password matches what is in the Dockerfile
