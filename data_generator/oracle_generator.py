9eea623612b7a78577fc5929aa4e6aed5fd00d02
Saurabh Jain <jainsaurabh78@gmail.com>
2020-08-04 15:21:47 +0530

    Oracle Support in dlx-test-sources

--- data_generator/oracle_generator.py
+++ data_generator/oracle_generator.py
@@ -1,3 +1,5 @@
+import random
+
 import cx_Oracle
 
 
@@ -132,8 +134,59 @@ class Oracle:
 
         return column_values
 
-    def generate_query(self, columns):
-        return {}
+    @staticmethod
+    def set_column_values(columns, loop_counter, fake):
+        random.seed(a=loop_counter, version=2)
+        row = {
+            'CUSTOMERKEY': str(columns['MaxCUSTOMERKEY'] + loop_counter),
+            'GEOGRAPHYKEY': str(random.choice(columns['GEOGRAPHYKEYs'])[0]),
+            'TITLE': '',
+            # 'TITLE': str(random.choice(columns['TITLEs'])[0]),
+            'FIRSTNAME': fake.first_name(),
+            'MIDDLENAME': random.choice([fake.random_uppercase_letter(), '']),
+            'LASTNAME': fake.last_name(),
+            'NAMESTYLE': str(random.choice(columns['NAMESTYLEs'])[0]),
+            'BIRTHDATE': fake.date_of_birth().strftime("%d-%b-%y 12:%M:%S.%f"),
+            'MARITALSTATUS': str(random.choice(columns['MARITALSTATUSs'])[0]),
+            # 'SUFFIX': str(random.choice(columns['SUFFIXs'])[0]),
+            'SUFFIX': '',
+            'GENDER': str(random.choice(columns['GENDERs'])[0]),
+            'YEARLYINCOME': str(random.choice(columns['YEARLYINCOMEs'])[0]),
+            'TOTALCHILDREN': str(random.choice(columns['TOTALCHILDRENs'])[0]),
+            'NUMBERCHILDRENATHOME': str(random.choice(columns['NUMBERCHILDRENATHOME'])[0]),
+            'ENGLISHEDUCATION': str(random.choice(columns['ENGLISHEDUCATIONs'])[0]),
+            'ENGLISHOCCUPATION': str(random.choice(columns['ENGLISHOCCUPATIONs'])[0]),
+            'HOUSEOWNERFLAG': str(random.choice(columns['HOUSEOWNERFLAGs'])[0]),
+            'NUMBERCARSOWNED': str(random.choice(columns['NUMBERCARSOWNEDs'])[0]),
+            'ADDRESSLINE1': fake.street_address(),
+            'ADDRESSLINE2': '',
+            'PHONE': fake.msisdn(),
+            'DATEFIRSTPURCHASE': fake.date('%d-%b-%y 12:%M:%S.%f'),
+            'COMMUTEDISTANCE': str(random.choice(columns['COMMUTEDISTANCEs'])[0]),
+        }
+        row['CUSTOMERALTERNATEKEY'] = f"AW{int(row['CUSTOMERKEY']):08d}"
+        row['EMAILADDRESS'] = str(row['FIRSTNAME']).lower() + str(random.choice(range(1, 101))) + "adventure-works.com"
+        return row
+
+    def generate_query(self, row):
+        sql_query = f"""
+        INSERT INTO \"{self.schema}\".\"{self.table}\" 
+        (CUSTOMERKEY, GEOGRAPHYKEY, CUSTOMERALTERNATEKEY, TITLE, FIRSTNAME, MIDDLENAME, LASTNAME, NAMESTYLE, 
+        BIRTHDATE, MARITALSTATUS, SUFFIX, GENDER, EMAILADDRESS, YEARLYINCOME, TOTALCHILDREN, 
+        NUMBERCHILDRENATHOME, ENGLISHEDUCATION, ENGLISHOCCUPATION, HOUSEOWNERFLAG, NUMBERCARSOWNED, ADDRESSLINE1, 
+        ADDRESSLINE2, PHONE, DATEFIRSTPURCHASE, COMMUTEDISTANCE) 
+        VALUES 
+        ({row['CUSTOMERKEY']}, {row['GEOGRAPHYKEY']}, '{row['CUSTOMERALTERNATEKEY']}', 
+        '{row['TITLE']}', '{row['FIRSTNAME']}', 
+        '{row['MIDDLENAME']}', 
+        '{row['LASTNAME']}', '{row['NAMESTYLE']}', TO_TIMESTAMP('{row['BIRTHDATE']}', 'DD-MON-RR HH.MI.SS.FF'), 
+        '{row['MARITALSTATUS']}', '{row['SUFFIX']}', 
+        '{row['GENDER']}', '{row['EMAILADDRESS']}', '{row['YEARLYINCOME']}', '{row['TOTALCHILDREN']}', 
+        '{row['NUMBERCHILDRENATHOME']}', '{row['ENGLISHEDUCATION']}', '{row['ENGLISHOCCUPATION']}', 
+        '{row['HOUSEOWNERFLAG']}', '{row['NUMBERCARSOWNED']}', '{row['ADDRESSLINE1']}', '{row['ADDRESSLINE2']}', 
+        '{row['PHONE']}', TO_TIMESTAMP('{row['DATEFIRSTPURCHASE']}', 'DD-MON-RR HH.MI.SS.FF'), '{row['COMMUTEDISTANCE']}')
+        """
+        return sql_query
 
     def insert_and_commit(self, sql_query):
         self.__run_query(sql_query)
