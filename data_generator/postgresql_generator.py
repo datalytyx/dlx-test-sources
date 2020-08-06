d8b5cd3fa4ba834dc5d1ed6c068ead0815b92578
Saurabh Jain <jainsaurabh78@gmail.com>
2020-08-06 19:06:51 +0530

    Added Support for Postgresql

--- data_generator/postgresql_generator.py
+++ data_generator/postgresql_generator.py
@@ -30,107 +30,108 @@ class PostgreSQL:
         self.connection.close()
 
     def __run_query(self, sql_query):
-        return self.cursor.execute(sql_query)
+        self.cursor.execute(sql_query)
+        return self.cursor
 
     def get_column_values(self):
         column_values = {}
 
-        sql_query = f"SELECT MAX(SalesOrderID) FROM \"{self.schema}\".\"{self.table}\""
+        sql_query = f"SELECT MAX(salesorderid) FROM \"{self.schema}\".\"{self.table}\""
         column_values['MaxSalesOrderId'] = self.__run_query(sql_query).fetchone()[0]
 
-        sql_query = f"SELECT DISTINCT(RevisionNumber) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(revisionnumber) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE RevisionNumber IS NOT NULL ORDER BY 1"
         column_values['RevisionNumbers'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(OrderDate) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(orderdate) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE OrderDate IS NOT NULL ORDER BY 1"
         column_values['OrderDates'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(DueDate) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(duedate) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE DueDate IS NOT NULL ORDER BY 1"
         column_values['DueDates'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(ShipDate) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(shipdate) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE ShipDate IS NOT NULL ORDER BY 1"
         column_values['ShipDates'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(Status) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(status) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE Status IS NOT NULL ORDER BY 1"
         column_values['Statuss'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(OnlineOrderFlag) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(onlineorderflag) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE OnlineOrderFlag IS NOT NULL ORDER BY 1"
         column_values['OnlineOrderFlags'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(PurchaseOrderNumber) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(purchaseordernumber) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE PurchaseOrderNumber IS NOT NULL ORDER BY 1"
         column_values['PurchaseOrderNumbers'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(AccountNumber) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(accountnumber) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE AccountNumber IS NOT NULL ORDER BY 1"
         column_values['AccountNumbers'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(CustomerID) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(customerid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE CustomerID IS NOT NULL ORDER BY 1"
         column_values['CustomerIDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(SalesPersonID) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(salespersonid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE SalesPersonID IS NOT NULL ORDER BY 1"
         column_values['SalesPersonIDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(TerritoryID) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(territoryid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE TerritoryID IS NOT NULL ORDER BY 1"
         column_values['TerritoryIDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(BillToAddressID) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(billtoaddressid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE BillToAddressID IS NOT NULL ORDER BY 1"
         column_values['BillToAddressIDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(ShipToAddressID) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(shiptoaddressid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE ShipToAddressID IS NOT NULL ORDER BY 1"
         column_values['ShipToAddressIDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(ShipMethodID) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(shipmethodid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE ShipMethodID IS NOT NULL ORDER BY 1"
         column_values['ShipMethodIDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(CreditCardID) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(creditcardid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE CreditCardID IS NOT NULL ORDER BY 1"
         column_values['CreditCardIDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(CreditCardApprovalCode) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(creditcardapprovalcode) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE CreditCardApprovalCode IS NOT NULL ORDER BY 1"
         column_values['CreditCardApprovalCodes'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(CurrencyRateID) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(currencyrateid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE CurrencyRateID IS NOT NULL ORDER BY 1"
         column_values['CurrencyRateIDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(SubTotal) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(subtotal) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE SubTotal IS NOT NULL ORDER BY 1"
         column_values['SubTotals'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(TaxAmt) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(taxamt) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE TaxAmt IS NOT NULL ORDER BY 1"
         column_values['TaxAmts'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(Freight) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(freight) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE Freight IS NOT NULL ORDER BY 1"
         column_values['Freights'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(TotalDue) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(totaldue) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE TotalDue IS NOT NULL ORDER BY 1"
         column_values['TotalDues'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(Comment) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(comment) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE Comment IS NOT NULL ORDER BY 1"
         column_values['Comments'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(RowGuid) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(rowguid) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE RowGuid IS NOT NULL ORDER BY 1"
         column_values['RowGuids'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(ModifiedDate) FROM \"{self.schema}\".\"{self.table}\" " \
+        sql_query = f"SELECT DISTINCT(modifieddate) FROM \"{self.schema}\".\"{self.table}\" " \
                     f"WHERE ModifiedDate IS NOT NULL ORDER BY 1"
         column_values['ModifiedDates'] = self.__run_query(sql_query).fetchall()
 
@@ -142,8 +143,8 @@ class PostgreSQL:
             'MaxSalesOrderId': str(columns['MaxSalesOrderId'] + loop_counter),
             'RevisionNumber': str(fake.random_digit_not_null()),
             'OrderDate': 'now()',
-            'DueDates': 'now() + 1',
-            'ShipDate': 'now() + 2',
+            'DueDates': "now() + interval '1' day",
+            'ShipDate': "now() + interval '2' day",
             'Status': str(random.choice(columns['Statuss'])[0]),
             'OnlineOrderFlag': str(random.choice(columns['OnlineOrderFlags'])[0]),
             'PurchaseOrderNumber': str(fake.isbn13(separator="-")),
@@ -170,17 +171,18 @@ class PostgreSQL:
     # def generate_query(self, columns):
     def generate_query(self, row):
         sql_query = f"""
-        INSERT INTO {self.schema}.{self.table}
-        (MaxSalesOrderId, RevisionNumber, OrderDate, DueDates, ShipDate, Status, OnlineOrderFlag, PurchaseOrderNumber,
-        AccountNumber, CustomerID, SalesPersonID, TerritoryID, BillToAddressID, ShipToAddressID, ShipMethodID,
-        CreditCardID, CreditCardApprovalCode, CurrencyRateID, SubTotal, Comment, RowGuid, ModifiedDate)
+        INSERT INTO "{self.schema}"."{self.table}"
+        (salesorderid, revisionnumber, orderdate, duedate, shipdate, status, onlineorderflag, purchaseordernumber,
+        accountnumber, customerid, salespersonid, territoryid, billtoaddressid, shiptoaddressid, shipmethodid,
+        creditcardid, creditcardapprovalcode, currencyrateid, subtotal, taxamt, freight, totaldue, comment, rowguid, 
+        modifieddate)
         VALUES
-        ('{row['MaxSalesOrderId']}', '{row['RevisionNumber']}', '{row['OrderDate']}', '{row['DueDates']}',
-        '{row['ShipDate']}', '{row['Status']}', '{row['OnlineOrderFlag']}', '{row['PurchaseOrderNumber']}',
+        ('{row['MaxSalesOrderId']}', '{row['RevisionNumber']}', {row['OrderDate']}, {row['DueDates']},
+        {row['ShipDate']}, '{row['Status']}', '{row['OnlineOrderFlag']}', '{row['PurchaseOrderNumber']}',
         '{row['AccountNumber']}', '{row['CustomerID']}', '{row['SalesPersonID']}', '{row['TerritoryID']}',
         '{row['BillToAddressID']}', '{row['ShipToAddressID']}', '{row['ShipMethodID']}', '{row['CreditCardID']}',
-        '{row['CreditCardApprovalCode']}', '{row['CurrencyRateID']}', '{row['SubTotal']}', '{row['Comment']}',
-        '{row['RowGuid']}', '{row['ModifiedDate']}')
+        '{row['CreditCardApprovalCode']}', '{row['CurrencyRateID']}', '{row['SubTotal']}', '{row['TaxAmt']}', 
+        '{row['Freight']}', '{row['TotalDue']}', '{row['Comment']}', '{row['RowGuid']}', {row['ModifiedDate']})
         """
         return sql_query
 
