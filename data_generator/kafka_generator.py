6d36f2492f75bfda2c24b0288929b64e555c3d1a
Saurabh Jain <jainsaurabh78@gmail.com>
2020-08-28 10:53:41 +0530

    Added support for MySQL and Kafka in new data-generator

--- data_generator/kafka_generator.py
+++ data_generator/kafka_generator.py
@@ -1,6 +1,6 @@
 import json
 
-from kafka import KafkaProducer, KafkaConsumer
+from kafka import KafkaProducer, KafkaConsumer, TopicPartition
 
 
 class Kafka:
@@ -22,14 +22,58 @@ class Kafka:
                              enable_auto_commit=False,
                              auto_offset_reset='earliest')
 
-    def run_query(self, sql_query):
-        pass
-
-    def generate_column_values(self, loop_counter):
+    def get_column_values(self):
+        topic_partition = TopicPartition(topic=self.topic, partition=0)
+        self.consumer.assign([topic_partition])
+        self.consumer.seek_to_end(topic_partition)
+        last_offset = self.consumer.position(topic_partition)
+        if last_offset != 0:
+            self.consumer.seek_to_beginning(topic_partition)
+            for msg in self.consumer:
+                if msg.offset == last_offset - 1:
+                    break
+            return msg.value
         return {}
 
-    def generate_query(self, columns):
-        return {}
+    def set_column_values(self, columns, loop_counter, faker):
+        if columns:
+            generated_message = self.__get_template_message(loop_counter + columns['ID'], faker)
+        else:
+            generated_message = self.__get_template_message(loop_counter, faker)
+        return generated_message
 
-    def insert_and_commit(self, sql_query):
-        self.run_query(sql_query)
+    def generate_query(self, row):
+        return row
+
+    def insert_and_commit(self, message):
+        self.producer.send(self.topic, message)
+        self.producer.flush()
+
+    @staticmethod
+    def __get_template_message(row_id, faker):
+        """
+        Returns a template message with random data
+        :param row_id: Value to be used as 'ID' for message
+        :return: Dict of random Data
+        """
+        return {
+            'ID': row_id,
+            'Name': {
+                'FirstName': faker.first_name(),
+                'LastName': faker.last_name(),
+            },
+            'Address': {
+                'StreetAddress': faker.address(),
+                'City': faker.city(),
+                'PostalCode': int(faker.postalcode()),
+                'Country': faker.country(),
+            },
+            'DateOfBirth': {
+                'Day': int(faker.day_of_month()),
+                'Month': faker.month_name(),
+                'Year': int(faker.year()),
+            },
+            'Email': faker.ascii_safe_email(),
+            'Job': faker.job(),
+        }
+
