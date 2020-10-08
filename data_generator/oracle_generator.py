b5e35bb2753d67179f6846b527e2ce9cd526875f
Saurabh Jain <jainsaurabh78@gmail.com>
2020-10-08 08:25:35 +0530

    Incremental data insertion support in table DIMEMPLOYEE for Oracle

--- data_generator/oracle_generator.py
+++ data_generator/oracle_generator.py
@@ -31,8 +31,7 @@ class Oracle:
     def __run_query(self, sql_query):
         return self.cursor.execute(sql_query)
 
-    def get_column_values(self):
-        column_values = {}
+    def get_dimcustomer(self, column_values):
         sql_query = f"SELECT MAX(CUSTOMERKEY) from \"{self.schema}\".\"{self.table}\""
         column_values['MaxCUSTOMERKEY'] = self.__run_query(sql_query).fetchone()[0]
 
@@ -48,33 +47,13 @@ class Oracle:
                     f"WHERE TITLE IS NOT NULL ORDER BY 1"
         column_values['TITLEs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(FIRSTNAME) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE FIRSTNAME IS NOT NULL ORDER BY 1"
-        column_values['FIRSTNAMEs'] = self.__run_query(sql_query).fetchall()
-
-        sql_query = f"SELECT DISTINCT(MIDDLENAME) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE MIDDLENAME IS NOT NULL ORDER BY 1"
-        column_values['MIDDLENAMEs'] = self.__run_query(sql_query).fetchall()
-
-        sql_query = f"SELECT DISTINCT(LASTNAME) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE LASTNAME IS NOT NULL ORDER BY 1"
-        column_values['LASTNAMEs'] = self.__run_query(sql_query).fetchall()
-
         sql_query = f"SELECT DISTINCT(NAMESTYLE) FROM \"{self.schema}\".{self.table} " \
                     f"WHERE NAMESTYLE IS NOT NULL ORDER BY 1"
         column_values['NAMESTYLEs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(BIRTHDATE) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE BIRTHDATE IS NOT NULL ORDER BY 1"
-        column_values['BIRTHDATEs'] = self.__run_query(sql_query).fetchall()
-
         sql_query = f"SELECT DISTINCT(MARITALSTATUS) FROM \"{self.schema}\".{self.table} " \
                     f"WHERE MARITALSTATUS IS NOT NULL ORDER BY 1"
         column_values['MARITALSTATUSs'] = self.__run_query(sql_query).fetchall()
-
-        sql_query = f"SELECT DISTINCT(SUFFIX) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE SUFFIX IS NOT NULL ORDER BY 1"
-        column_values['SUFFIXs'] = self.__run_query(sql_query).fetchall()
 
         sql_query = f"SELECT DISTINCT(GENDER) FROM \"{self.schema}\".{self.table} " \
                     f"WHERE GENDER IS NOT NULL ORDER BY 1"
@@ -112,30 +91,77 @@ class Oracle:
                     f"WHERE NUMBERCARSOWNED IS NOT NULL ORDER BY 1"
         column_values['NUMBERCARSOWNEDs'] = self.__run_query(sql_query).fetchall()
 
-        sql_query = f"SELECT DISTINCT(ADDRESSLINE1) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE ADDRESSLINE1 IS NOT NULL ORDER BY 1"
-        column_values['ADDRESSLINE1s'] = self.__run_query(sql_query).fetchall()
-
-        sql_query = f"SELECT DISTINCT(ADDRESSLINE2) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE ADDRESSLINE2 IS NOT NULL ORDER BY 1"
-        column_values['ADDRESSLINE2s'] = self.__run_query(sql_query).fetchall()
-
-        sql_query = f"SELECT DISTINCT(PHONE) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE PHONE IS NOT NULL ORDER BY 1"
-        column_values['PHONEs'] = self.__run_query(sql_query).fetchall()
-
-        sql_query = f"SELECT DISTINCT(DATEFIRSTPURCHASE) FROM \"{self.schema}\".{self.table} " \
-                    f"WHERE DATEFIRSTPURCHASE IS NOT NULL ORDER BY 1"
-        column_values['DATEFIRSTPURCHASEs'] = self.__run_query(sql_query).fetchall()
-
         sql_query = f"SELECT DISTINCT(COMMUTEDISTANCE) FROM \"{self.schema}\".{self.table} " \
                     f"WHERE COMMUTEDISTANCE IS NOT NULL ORDER BY 1"
         column_values['COMMUTEDISTANCEs'] = self.__run_query(sql_query).fetchall()
 
         return column_values
 
+    def get_dimemployee(self, column_values):
+        sql_query = f"SELECT MAX(EMPLOYEEKEY) from \"{self.schema}\".\"{self.table}\""
+        column_values['MaxEMPLOYEEKEY'] = self.__run_query(sql_query).fetchone()[0]
+
+        sql_query = f"SELECT DISTINCT(PARENTEMPLOYEEKEY) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE PARENTEMPLOYEEKEY IS NOT NULL ORDER BY 1"
+        column_values['PARENTEMPLOYEEKEYs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(EMPLOYEENATIONALIDALTERNATEKEY) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE EMPLOYEENATIONALIDALTERNATEKEY IS NOT NULL ORDER BY 1"
+        column_values['EMPLOYEENATIONALIDALTERNATEKEYs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(PARENTEMPLOYEENATIONALIDALTKEY) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE PARENTEMPLOYEENATIONALIDALTKEY IS NOT NULL ORDER BY 1"
+        column_values['PARENTEMPLOYEENATIONALIDALTKEYs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(SALESTERRITORYKEY) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE SALESTERRITORYKEY IS NOT NULL ORDER BY 1"
+        column_values['SALESTERRITORYKEYs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(FIRSTNAME) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE FIRSTNAME IS NOT NULL ORDER BY 1"
+        column_values['FIRSTNAMEs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(TITLE) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE TITLE IS NOT NULL ORDER BY 1"
+        column_values['TITLEs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(SALARIEDFLAG) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE SALARIEDFLAG IS NOT NULL ORDER BY 1"
+        column_values['SALARIEDFLAGs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(PAYFREQUENCY) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE PAYFREQUENCY IS NOT NULL ORDER BY 1"
+        column_values['PAYFREQUENCYs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(BASERATE) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE BASERATE IS NOT NULL ORDER BY 1"
+        column_values['BASERATEs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(VACATIONHOURS) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE VACATIONHOURS IS NOT NULL ORDER BY 1"
+        column_values['VACATIONHOURSs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(SICKLEAVEHOURS) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE SICKLEAVEHOURS IS NOT NULL ORDER BY 1"
+        column_values['SICKLEAVEHOURSs'] = self.__run_query(sql_query).fetchall()
+
+        sql_query = f"SELECT DISTINCT(DEPARTMENTNAME) FROM \"{self.schema}\".{self.table} " \
+                    f"WHERE DEPARTMENTNAME IS NOT NULL ORDER BY 1"
+        column_values['DEPARTMENTNAMEs'] = self.__run_query(sql_query).fetchall()
+
+        return column_values
+
+    def get_column_values(self):
+        column_values = {}
+        if self.table.lower() == 'dimcustomer':
+            self.get_dimcustomer(column_values)
+        elif self.table.lower() == 'dimemployee':
+            self.get_dimemployee(column_values)
+
+        return column_values
+
     @staticmethod
-    def set_column_values(columns, loop_counter, fake):
+    def set_dimcustomer(columns, loop_counter, fake):
         random.seed(a=loop_counter, version=2)
         row = {
             'CUSTOMERKEY': str(columns['MaxCUSTOMERKEY'] + loop_counter),
@@ -165,10 +191,55 @@ class Oracle:
             'COMMUTEDISTANCE': str(random.choice(columns['COMMUTEDISTANCEs'])[0]),
         }
         row['CUSTOMERALTERNATEKEY'] = f"AW{int(row['CUSTOMERKEY']):08d}"
-        row['EMAILADDRESS'] = str(row['FIRSTNAME']).lower() + str(random.choice(range(1, 101))) + "adventure-works.com"
+        row['EMAILADDRESS'] = str(row['FIRSTNAME']).lower() + str(random.choice(range(1, 101))) + "@adventure-works.com"
         return row
 
-    def generate_query(self, row):
+    @staticmethod
+    def set_dimemployee(columns, loop_counter, fake):
+        random.seed(a=loop_counter, version=2)
+        row = {
+            'EMPLOYEEKEY': str(columns['MaxEMPLOYEEKEY'] + loop_counter),
+            'PARENTEMPLOYEEKEY': str(random.choice(columns['PARENTEMPLOYEEKEYs'])[0]),
+            'EMPLOYEENATIONALIDALTERNATEKEY': str(random.choice(columns['EMPLOYEENATIONALIDALTERNATEKEYs'])[0]),
+            'PARENTEMPLOYEENATIONALIDALTKEY': str(random.choice(columns['PARENTEMPLOYEENATIONALIDALTKEYs'])[0]),
+            'SALESTERRITORYKEY': str(random.choice(columns['SALESTERRITORYKEYs'])[0]),
+            'FIRSTNAME': fake.first_name(),
+            'LASTNAME': fake.last_name(),
+            'MIDDLENAME': random.choice([fake.random_uppercase_letter(), '']),
+            'NAMESTYLE': 0,
+            'TITLE': str(random.choice(columns['TITLEs'])[0]),
+            'HIREDATE': fake.date_of_birth().strftime("%y%m%d"),
+            'BIRTHDATE': fake.date_of_birth().strftime("%y%m%d"),
+            'PHONE': fake.msisdn(),
+            'MARITALSTATUS': str(random.choice(['M', 'S'])[0]),
+            'EMERGENCYCONTACTNAME': f"{fake.first_name()} {fake.last_name()}",
+            'EMERGENCYCONTACTPHONE': fake.msisdn(),
+            'SALARIEDFLAG': str(random.choice(columns['SALARIEDFLAGs'])[0]),
+            'GENDER': str(random.choice(['M', 'F'])[0]),
+            'PAYFREQUENCY': str(random.choice(columns['PAYFREQUENCYs'])[0]),
+            'BASERATE': str(random.choice(columns['BASERATEs'])[0]),
+            'VACATIONHOURS': str(random.choice(columns['VACATIONHOURSs'])[0]),
+            'SICKLEAVEHOURS': str(random.choice(columns['SICKLEAVEHOURSs'])[0]),
+            'CURRENTFLAG': 1,
+            'SALESPERSONFLAG': 0,
+            'DEPARTMENTNAME': str(random.choice(columns['DEPARTMENTNAMEs'])[0]),
+            'STARTDATE': fake.date_of_birth().strftime("%y%m%d"),
+            'STATUS': 'CURRENT',
+        }
+        row['LOGINID'] = f"adventure-works\{row['FIRSTNAME']}{str(random.choice(range(1, 101)))}"
+        row['EMAILADDRESS'] = str(row['FIRSTNAME']).lower() + str(random.choice(range(1, 101))) + "@adventure-works.com"
+        return row
+
+    def set_column_values(self, columns, loop_counter, fake):
+        row = {}
+        if self.table.lower() == 'dimcustomer':
+            row = self.set_dimcustomer(columns, loop_counter, fake)
+        elif self.table.lower() == 'dimemployee':
+            row = self.set_dimemployee(columns, loop_counter, fake)
+
+        return row
+
+    def query_dimcustomer(self, row):
         sql_query = f"""
         INSERT INTO \"{self.schema}\".\"{self.table}\" 
         (CUSTOMERKEY, GEOGRAPHYKEY, CUSTOMERALTERNATEKEY, TITLE, FIRSTNAME, MIDDLENAME, LASTNAME, NAMESTYLE, 
@@ -186,6 +257,35 @@ class Oracle:
         '{row['HOUSEOWNERFLAG']}', '{row['NUMBERCARSOWNED']}', '{row['ADDRESSLINE1']}', '{row['ADDRESSLINE2']}', 
         '{row['PHONE']}', TO_TIMESTAMP('{row['DATEFIRSTPURCHASE']}', 'DD-MON-RR HH.MI.SS.FF'), '{row['COMMUTEDISTANCE']}')
         """
+        return sql_query
+
+    def query_dimemployee(self, row):
+        sql_query = f"""
+        INSERT INTO \"{self.schema}\".\"{self.table}\" 
+        (EMPLOYEEKEY, PARENTEMPLOYEEKEY, EMPLOYEENATIONALIDALTERNATEKEY, PARENTEMPLOYEENATIONALIDALTKEY, 
+        SALESTERRITORYKEY, FIRSTNAME, LASTNAME, MIDDLENAME, NAMESTYLE, TITLE, HIREDATE, BIRTHDATE, LOGINID, 
+        EMAILADDRESS, PHONE, MARITALSTATUS, EMERGENCYCONTACTNAME, EMERGENCYCONTACTPHONE, SALARIEDFLAG, GENDER,
+        PAYFREQUENCY, BASERATE, VACATIONHOURS, SICKLEAVEHOURS, CURRENTFLAG, SALESPERSONFLAG, DEPARTMENTNAME, 
+        STARTDATE, STATUS) 
+        VALUES 
+        ({row['EMPLOYEEKEY']}, {row['PARENTEMPLOYEEKEY']}, '{row['EMPLOYEENATIONALIDALTERNATEKEY']}', 
+        '{row['PARENTEMPLOYEENATIONALIDALTKEY']}', {row['SALESTERRITORYKEY']}, '{row['FIRSTNAME']}', 
+        '{row['LASTNAME']}', '{row['MIDDLENAME']}', {row['NAMESTYLE']}, '{row['TITLE']}', {row['HIREDATE']}, 
+        {row['BIRTHDATE']}, '{row['LOGINID']}', '{row['EMAILADDRESS']}', '{row['PHONE']}', '{row['MARITALSTATUS']}', 
+        '{row['EMERGENCYCONTACTNAME']}', '{row['EMERGENCYCONTACTPHONE']}', {row['SALARIEDFLAG']}, '{row['GENDER']}', 
+        {row['PAYFREQUENCY']}, {row['BASERATE']}, {row['VACATIONHOURS']}, {row['SICKLEAVEHOURS']}, 
+        {row['CURRENTFLAG']}, {row['SALESPERSONFLAG']}, '{row['DEPARTMENTNAME']}', {row['STARTDATE']}, 
+        '{row['STATUS']}')
+        """
+        return sql_query
+
+    def generate_query(self, row):
+        sql_query = ""
+        if self.table.lower() == 'dimcustomer':
+            sql_query = self.query_dimcustomer(row)
+        elif self.table.lower() == 'dimemployee':
+            sql_query = self.query_dimemployee(row)
+
         return sql_query
 
     def insert_and_commit(self, sql_query):
